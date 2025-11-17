37913d8866368792e254679d3034097d2d08bebe
Mohit Rathore <mohit.code2023@gmail.com>
2025-11-17 14:14:23 +0530

    Initial Project Setup for Splurge App

---
+++ src/MainApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const MainApp = () => {
+    return(
+        <View>
+            <Text style={{fontSize:25}}>Splurge App</Text>
+        </View>
+    )
+}
+
+export default MainApp;
